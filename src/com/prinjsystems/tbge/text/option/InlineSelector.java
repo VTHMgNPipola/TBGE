432e51e5af94149f99c252394a0eb8bed92cb336
VTHMgNPipola <web.anunciante@gmail.com>
2018-03-22 11:51:31 -0300

    Finished coding InlineSelector, need to test it.

--- src/com/prinjsystems/tbge/text/option/InlineSelector.java
+++ src/com/prinjsystems/tbge/text/option/InlineSelector.java
@@ -16,11 +16,62 @@
  */
 package com.prinjsystems.tbge.text.option;
 
+import com.prinjsystems.tbge.text.TerminalPrinter;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * This implementation of Selector interface prints the helping text, and then
  * all the options in lowercase, inside brackets, and the default option is printed
  * in uppercase.
  */
-public class InlineSelector {
-
+public class InlineSelector implements Selector {
+	private List<String> options;
+	private TerminalPrinter tp;
+	private int defaultOption;
+	private String text;
+	
+	/**
+	 * Creates a default instance of InlineSelector, that should have
+	 * a {@code TerminalPrinter} as argument, to be used as the print
+	 * tool.
+	 * @param tp Print tool used to print the text and the options on the screen.
+	 */
+	public InlineSelector(TerminalPrinter tp) {
+		this.tp = tp;
+		defaultOption = 0;
+	}
+	
+	@Override
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	@Override
+	public void setOptions(List<String> options) {
+		this.options = options;
+	}
+	
+	@Override
+	public void setDefaultOption(int optionIndex) {
+		defaultOption = optionIndex;
+	}
+	
+	@Override
+	public int askSelector() {
+		String separator = "";
+		Scanner s = new Scanner(System.in);
+		tp.println();
+		tp.print(text + ": [");
+		for(int i = 0; i < options.size(); i++) {
+			if(i == defaultOption) {
+				tp.print(separator + options.get(i).toUpperCase());
+			} else {
+				tp.print(separator + options.get(i).toLowerCase());
+			}
+			separator = ", ";
+		}
+		tp.print("]: ");
+		return Integer.parseInt(s.nextLine());
+	}
 }
